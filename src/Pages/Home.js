8400b5c64aa939e346c68059b31818003cec7e3f
Arrtea21 <arpit.khatrirols56@gmail.com>
2024-06-04 19:27:35 +0530

    content changes

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -11,7 +11,8 @@ export default function Home(){
             <Tea></Tea>
             <Links></Links>
             <div>
-                <p className='d-flex align-items-center justify-content-center p-4 pb-0 mb-0 text-center w-100 text-white' style={{backgroundColor:"black",fontFamily: "monospace"}}>
+                <p className='d-flex align-items-center justify-content-center p-4 py-0 mb-0 text-center w-100 text-white' style={{backgroundColor:"black",fontFamily: "monospace"}}>Hey there, I'm Arrtea, but Tea works just fine.</p>
+                <p className='d-flex align-items-center justify-content-center pb-4 mb-0 text-center w-100 text-white' style={{backgroundColor:"black",fontFamily: "monospace"}}>
                 Just goofing around on the internet. Into everything tech-related, plus games, music, anime, and did I mention games 😆? Designated tech guy for the family. Loves a good story.</p>
                 <a href='https://discord.com/users/425208565276475392' target='_blank' ><p className='d-flex align-items-center justify-content-center p-0 m-0 mb-5 text-center w-100 text-white' style={{backgroundColor:"black",fontFamily: "monospace"}}> Contact me on discord anytime ♥ . </p></a>
             </div>
